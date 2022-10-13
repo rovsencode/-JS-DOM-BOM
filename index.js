33f3ac0dab8f5f8941e0805115f09ff554d2dc29
rovsencode <rovshanakh@code.edu.az>
2022-10-13 23:34:47 +0400

    update JsDom

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const btn = document.querySelector(".btn")
 btn.addEventListener("click", () => {
     const fn = document.querySelector("#name").value
     const ln = document.querySelector("#surname").value
-    const em = document.querySelector("#email").value
-    if (fn === null) {
+    const em = document.querySelector(".email").value
+    if (fn.length<1 ) {
             document.getElementById("first-name").innerHTML = "wrong input,please make correct"
     }
     else {
          document.getElementById("first-name").innerHTML = fn;
     }
-    if (ln === null) {
+    if (ln.length<1) {
                 document.getElementById("last-name").innerHTML="wrong input,please make correct"
 
     }
     else {
         document.getElementById("last-name").innerHTML = ln;
     }
-       if (em === null) {
+       if (em.length<1) {
               document.getElementById("email").innerHTML="wrong input,please make correct "
         
     }
